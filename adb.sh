5cc7b949a5c118ba3f93874a3872d5b809e232be
STARTX-HACKER <130206716+STARTX-HACKER@users.noreply.github.com>
2023-04-10 05:44:16 +0200

    Update adb.sh

--- adb.sh
+++ adb.sh
@@ -1,4 +1,5 @@
-​cd​ ​~​/adb-shell
-​mv /data/data/com.termux/files/home/adb-shell/adb /data/data/com.termux/files/usr/bin/
-​chmod +x /data/data/com.termux/files/usr/bin/adb
-​rm -rf adb-shell
+cd ~/adb-shell
+mv adb /data/data/com.termux/files/usr/bin
+chmod +x /data/data/com.termux/files/usr/bin/adb
+rm -rf ~/adb-shell
+cd
