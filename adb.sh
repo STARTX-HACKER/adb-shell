1243847e72692232d21fcdbfb2686727c7f188f3
STARTX HACKER <130206716+STARTX-HACKER@users.noreply.github.com>
2023-05-03 20:47:02 +0300

    Update adb.sh

--- adb.sh
+++ adb.sh
@@ -1,7 +1,6 @@
 [ -e "rish" ] && rm $HOME/rish
 [ -e "rish_shizuku.dex" ] && mv $HOME/rish_shizuku.dex .adb.dex
 
-cd ~/adb-shell
-mv adb /data/data/com.termux/files/usr/bin
-chmod +x /data/data/com.termux/files/usr/bin/adb
+mv ~/adb-shell/adb ~/../usr/bin
+chmod +x ~/../usr/bin/adb
 rm -rf ~/adb-shell
