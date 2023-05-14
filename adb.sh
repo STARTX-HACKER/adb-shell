a690c72834542ab1b05703aa97bdfdc726dc3206
STARTX HACKER <130206716+STARTX-HACKER@users.noreply.github.com>
2023-05-15 01:42:23 +0300

    Update adb.sh

--- adb.sh
+++ adb.sh
@@ -1,8 +1,7 @@
 [ -e "$HOME/rish" ] && rm $HOME/rish
-[ -e "$HOME/rish_shizuku.dex" ] && mv $HOME/rish_shizuku.dex $HOME/adb.dex
 
-mv $HOME/adb-shell/adb $PREFIX/bin/
+wget -P $PREFIX/bin/ https://github.com/STARTX-HACKER/adb-shell/blob/main/adb
 chmod +x $PREFIX/bin/adb
+
 [ -e "$PREFIX/opt/adb-shell" ] || mkdir -p $PREFIX/opt/adb-shell
-[ -e "$HOME/adb.dex" ] && mv $HOME/adb.dex $PREFIX/opt/adb-shell/
-rm -rf $HOME/adb-shell
+[ -e "$HOME/adb.dex" ] && mv $HOME/rish_shizuku.dex $PREFIX/opt/adb-shell/adb.dex
