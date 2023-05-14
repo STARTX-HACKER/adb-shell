70be3172d4b2b47b8abfc7a1a5427f63c81a90f8
STARTX HACKER <130206716+STARTX-HACKER@users.noreply.github.com>
2023-05-15 01:31:03 +0300

    Update adb.sh

--- adb.sh
+++ adb.sh
@@ -6,4 +6,3 @@ chmod +x $PREFIX/bin/adb
 [ -e "$PREFIX/opt/adb-shell" ] || mkdir -p $PREFIX/opt/adb-shell
 [ -e "$HOME/adb.dex" ] && mv $HOME/adb.dex $PREFIX/opt/adb-shell/
 rm -rf $HOME/adb-shell
-exit
